0acc6d125187d09baec364778beccf787c1dde7b
elvis-ndubuisi <provictor.ie@gmail.com>
2023-04-13 10:24:16 +0100

    Svelte init

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main class="min-h-screen">
+
+</main>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
