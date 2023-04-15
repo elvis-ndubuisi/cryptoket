9c8a51193b638ae826b31aceeca0d79ddc35359d
elvis-ndubuisi <provictor.ie@gmail.com>
2023-04-15 18:23:02 +0100

    style guide update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "cr-red-violet": "#DA18A3",
+        "cr-red-linear": "#EB1484 - #C81CC5",
+        "cr-dark": "#24252D",
+        "cr-light": "#FFFFFF",
+        "cr-grey-100": "#E3E1E3",
+        "cr-grey-200": "#888888",
+        "cr-grey-300": "#4F4F4F",
+        "cr-black-100": "#2D2E36",
+        "cr-black-200": "#1B1A21",
+        "cr-black-300": "#E3E1E3",
+        "cr-black-400": "#24252D",
+      },
+      fontSize: {
+        "cr-h1": "28px",
+        "cr-h2": "24px",
+        "cr-h3": "20px",
+      },
+    },
   },
   plugins: [],
 };
