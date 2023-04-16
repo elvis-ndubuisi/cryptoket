f188ee1d1bbace28eea716de342a4888e99920ef
elvis-ndubuisi <provictor.ie@gmail.com>
2023-04-16 08:29:17 +0100

    Added 404 page

---
+++ src/routes/NotFound.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { useNavigate } from "svelte-navigator";
+
+  const navigate = useNavigate();
+</script>
+
+<section class="h-screen grid place-content-center gap-4">
+  <h1 class="text-3xl font-semibold">Sorry! Page not available</h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, doloremque!
+  </p>
+  <button
+    type="button"
+    class="font-bold capitalize"
+    on:click={() => {
+      navigate("/", { replace: true });
+    }}>Back to Home</button
+  >
+</section>
