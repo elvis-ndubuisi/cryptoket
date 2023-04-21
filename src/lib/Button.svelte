159883f6efcf23d6cc18f1c204b99e6319d2fa7c
elvis-ndubuisi <provictor.ie@gmail.com>
2023-04-21 13:26:58 +0100

    Added styles options for Button.svelte

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -3,6 +3,7 @@
 
   export let type: "button" | "submit" | "submit" = "button";
   export let outline: boolean = false;
+  export let styles: string = "";
   // export let name: string = "button";
 
   const dispatch = createEventDispatcher();
@@ -16,11 +17,11 @@
 <button
   on:click={onclick}
   {type}
-  class={`border-none capitalize rounded-lg font-semibold text-sm py-2 px-7 ${
+  class={`border-none capitalize rounded-lg font-semibold text-sm py-2 w-full min-w-[100px] h-[inherit] ${
     outline
       ? "text-cr-red-violet bg-transparent "
       : "text-cr-light bg-gradient-to-tr from-cr-red-linear-start to-cr-red-linear-end"
-  }`}
+  } ${styles}`}
 >
   <slot />
 </button>
