2806d32f571e10ac6e1577994d7976c45da41023
elvis-ndubuisi <provictor.ie@gmail.com>
2023-04-21 13:04:11 +0100

    Modal components

---
+++ src/lib/modal/Checkout.svelte
@@ -0,0 +1,8 @@
+<section class="flex flex-col">
+  <h2 class="semibold text-2xl text-center cpaitalize">check out</h2>
+  <hr class="border border-cr-grey-300" />
+  <div class="flex items-center justify-between">
+    <p>item</p>
+    <p>subtotal</p>
+  </div>
+</section>
--- src/lib/modal/Modal.svelte
+++ src/lib/modal/Modal.svelte
@@ -6,6 +6,8 @@
   <section
     class="absolute top-0 left-0 w-screen h-screen bg-cr-black-200 opacity-90 grid items-center justify-center"
   >
-    <slot />
+    <section class="bg-cr-black-100 w-[586px] rounded-[20px] p-5">
+      <slot />
+    </section>
   </section>
 {/if}
