f824a8c2e141fcd7ea7497327fd3ada5f1d01723
elvis-ndubuisi <provictor.ie@gmail.com>
2023-04-23 01:48:27 +0100

    Added : mock data

---
+++ src/data/mockData.js
@@ -0,0 +1,50 @@
+export const nftMock = [
+  {
+    artUri: "../assets/shirt-viz-screenshot.png",
+    artName: "elvis midas",
+    artPrice: 23,
+    artLikes: 343,
+  },
+  {
+    artUri: "../assets/shirt-viz-screenshot.png",
+    artName: "midas gold",
+    artPrice: 23,
+    artLikes: 343,
+  },
+  {
+    artUri: "../assets/shirt-viz-screenshot.png",
+    artName: "elvis midas",
+    artPrice: 23,
+    artLikes: 343,
+  },
+  {
+    artUri: "../assets/shirt-viz-screenshot.png",
+    artName: "elvis midas",
+    artPrice: 23,
+    artLikes: 343,
+  },
+  {
+    artUri: "../assets/shirt-viz-screenshot.png",
+    artName: "elvis midas",
+    artPrice: 23,
+    artLikes: 343,
+  },
+  {
+    artUri: "../assets/shirt-viz-screenshot.png",
+    artName: "elvis midas",
+    artPrice: 23,
+    artLikes: 343,
+  },
+  {
+    artUri: "../assets/shirt-viz-screenshot.png",
+    artName: "elvis midas",
+    artPrice: 23,
+    artLikes: 343,
+  },
+  {
+    artUri: "../assets/shirt-viz-screenshot.png",
+    artName: "elvis midas",
+    artPrice: 23,
+    artLikes: 343,
+  },
+];
